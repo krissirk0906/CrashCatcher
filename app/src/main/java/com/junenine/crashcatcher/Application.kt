9f9d1ea638cff72a7e6aa0aa5ecc9f339ff1f476
Cao Thành Tâm <tam.ct@intelin.vn>
2024-11-14 11:07:19 +0700

    feat: improve crash handler

--- app/src/main/java/com/junenine/crashcatcher/Application.kt
+++ app/src/main/java/com/junenine/crashcatcher/Application.kt
@@ -2,8 +2,13 @@ package com.junenine.crashcatcher
 
 import android.app.Application
 
-class Application  : Application() {
+class Application : Application() {
+    private val crashHandler = CrashHandler.initialize(this)
+
     override fun onCreate() {
         super.onCreate()
+        crashHandler.setRestartActivity(
+            RestartActivity()
+        )
     }
 }
